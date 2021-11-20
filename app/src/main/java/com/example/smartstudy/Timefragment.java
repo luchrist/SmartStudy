e1f703cc10e8a555da9348e356df716588727dbb
luchrist <luca.christ@gmx.de>
2021-11-20 03:10:20 +0100

    Timefragment added timepickerdialog

--- app/src/main/java/com/example/smartstudy/Timefragment.java
+++ app/src/main/java/com/example/smartstudy/Timefragment.java
@@ -28,9 +28,20 @@ public class Timefragment extends Fragment implements View.OnClickListener {
         View view = inflater.inflate(R.layout.time_fragment, container, false);
 
         monTimePicker = view.findViewById(R.id.monTimePicker);
-        tueTimePicker
+        tueTimePicker = view.findViewById(R.id.tueTimePicker);
+        wedTimePicker = view.findViewById(R.id.wedTimePicker);
+        thuTimePicker = view.findViewById(R.id.thuTimePicker);
+        friTimePicker = view.findViewById(R.id.friTimePicker);
+        satTimePicker = view.findViewById(R.id.satTimePicker);
+        sunTimePicker = view.findViewById(R.id.sunTimePicker);
         monTimePicker.setOnClickListener(this);
-        
+        tueTimePicker.setOnClickListener(this);
+        wedTimePicker.setOnClickListener(this);
+        thuTimePicker.setOnClickListener(this);
+        friTimePicker.setOnClickListener(this);
+        satTimePicker.setOnClickListener(this);
+        sunTimePicker.setOnClickListener(this);
+
 
 
         return view;
@@ -44,7 +55,22 @@ public class Timefragment extends Fragment implements View.OnClickListener {
             public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
                 hour = selectedHour;
                 minute = selectedMinute;
-                monTimePicker.setText(String.format(Locale.getDefault(), "%02d:%02d", hour, minute));
+                if (monTimePicker.equals(view)) {
+                    monTimePicker.setText(String.format(Locale.getDefault(), "%02d:%02d", hour, minute));
+                } else if (tueTimePicker.equals(view)) {
+                    tueTimePicker.setText(String.format(Locale.getDefault(), "%02d:%02d", hour, minute));
+                }else if(view == wedTimePicker){
+                    wedTimePicker.setText(String.format(Locale.getDefault(), "%02d:%02d", hour, minute));
+                }else if (view == thuTimePicker){
+                    thuTimePicker.setText(String.format(Locale.getDefault(), "%02d:%02d", hour, minute));
+                }else if(view == friTimePicker){
+                    friTimePicker.setText(String.format(Locale.getDefault(), "%02d:%02d", hour, minute));
+                }else if(view == satTimePicker){
+                    satTimePicker.setText(String.format(Locale.getDefault(), "%02d:%02d", hour, minute));
+                }else if(view == sunTimePicker){
+                    sunTimePicker.setText(String.format(Locale.getDefault(), "%02d:%02d", hour, minute));
+                }
+
             }
         };
         int style = AlertDialog.THEME_HOLO_DARK;
