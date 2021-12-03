c393415bcb86d046702410fbecdfe7157e2cb807
luchrist <luca.christ@gmx.de>
2021-12-03 20:42:41 +0100

    languageFragment design

---
+++ app/src/main/java/com/example/smartstudy/Language.java
@@ -0,0 +1,38 @@
+package com.example.smartstudy;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link Language#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class Language extends Fragment {
+
+
+
+    public Language() {
+        // Required empty public constructor
+    }
+
+
+
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view =  inflater.inflate(R.layout.fragment_language, container, false);
+
+
+        return view;
+    }
+}
--- app/src/main/java/com/example/smartstudy/StudyFragment.java
+++ app/src/main/java/com/example/smartstudy/StudyFragment.java
@@ -1,10 +1,12 @@
 package com.example.smartstudy;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Button;
+import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -12,13 +14,44 @@ import androidx.fragment.app.Fragment;
 import org.jetbrains.annotations.NotNull;
 
 public class StudyFragment extends Fragment {
+    Button languages,math, informatic;
+
 
     @Nullable
     @org.jetbrains.annotations.Nullable
     @Override
     public View onCreateView(@NonNull @NotNull LayoutInflater inflater, @Nullable @org.jetbrains.annotations.Nullable ViewGroup container, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.study_fragment, container, false);
-        
+        languages= view.findViewById(R.id.languages);
+        math = view.findViewById(R.id.math);
+        informatic = view.findViewById(R.id.informatic);
+
+
+        languages.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.container,
+                        new Language()).commit();
+
+            }
+        });
+        math.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.container,
+                        new MathFragment()).commit();
+
+            }
+        });
+
+        informatic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.container,
+                        new InformaticFragment()).commit();
+
+            }
+        });
         return view;
     }
 }
