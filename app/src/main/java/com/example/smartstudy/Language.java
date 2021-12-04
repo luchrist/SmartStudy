992bd84e2b3fd97f931c75159dfb52f5d1854edc
luchrist <luca.christ@gmx.de>
2021-12-04 18:41:56 +0100

    addCollection

--- app/src/main/java/com/example/smartstudy/Language.java
+++ app/src/main/java/com/example/smartstudy/Language.java
@@ -7,32 +7,38 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Language#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class Language extends Fragment {
+public class Language extends Fragment implements View.OnClickListener {
 
-
-
-    public Language() {
-        // Required empty public constructor
-    }
-
-
-
-
-
-
+    public ImageButton backbtn;
+    public Button addCollection;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view =  inflater.inflate(R.layout.fragment_language, container, false);
 
+        backbtn = view.findViewById(R.id.backbtn);
+        addCollection = view.findViewById(R.id.addCollection);
+        backbtn.setOnClickListener(this);
+        addCollection.setOnClickListener(this);
+
 
         return view;
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == backbtn){
+            getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.container,
+                    new StudyFragment()).commit();
+        }else if(view == addCollection){
+            getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.container,
+                    new AddCollection()).commit();
+        }else{
+
+        }
     }
 }
