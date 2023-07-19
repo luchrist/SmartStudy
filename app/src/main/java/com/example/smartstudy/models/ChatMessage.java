83c1ab55a7c381eebbb6c188f1bf0b58d4478eb5
luch <luca.christ@gmx.de>
2023-07-19 20:51:52 +0200

    chatting works

--- app/src/main/java/com/example/smartstudy/models/ChatMessage.java
+++ app/src/main/java/com/example/smartstudy/models/ChatMessage.java
@@ -1,8 +1,10 @@
 package com.example.smartstudy.models;
 
+import java.util.Date;
 import java.util.List;
 
 public class ChatMessage {
     public String senderId, message, dateTime;
-    public List<String> receiversId;
+    public Date dateObject;
+    public String receiversId;
 }
--- app/src/main/java/com/example/smartstudy/utilities/Constants.java
+++ app/src/main/java/com/example/smartstudy/utilities/Constants.java
@@ -25,4 +25,5 @@ public class Constants {
     public static final String KEY_MSG = "message";
     public static final String KEY_TIMESTAMP = "timestamp";
 
+    public static final String KEY_RECEIVER = "receiver";
 }
