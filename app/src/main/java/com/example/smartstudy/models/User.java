de9ea0db5a5b3510510c34d9960315a34fd0eaf6
luchrist <luca.christ@gmx.de>
2023-08-06 20:58:32 +0200

    not the fault

--- app/src/main/java/com/example/smartstudy/models/User.java
+++ app/src/main/java/com/example/smartstudy/models/User.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class User implements Serializable {
 
-
+        public boolean availability;
         public String userName, email, image, fcmToken;
         public List<String> groupId;
 }
