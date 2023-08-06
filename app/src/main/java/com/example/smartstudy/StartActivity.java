bb55cbc64c4c70dfc71074dbdc19d16567a85b83
luchrist <luca.christ@gmx.de>
2023-08-06 20:57:13 +0200

    not the fault

--- app/src/main/java/com/example/smartstudy/StartActivity.java
+++ app/src/main/java/com/example/smartstudy/StartActivity.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
